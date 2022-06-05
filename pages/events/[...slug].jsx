94697912aeff568fffe46f44e13ef0935b1eac2b
Apurba Ghosh <new.to.world2003@gmail.com>
2022-06-06 00:13:30 +0530

    Final Lap (4th) Added Some new Components like Error Alert, Results Title etc. Stylised the New Component as well using CSS Modules. Refactored Some Components for Filtering the Data. Finished the Filtered Page. Handled the Base Cases for Routes in which even if some Wrong Route is injected the Page Displays Error. If the Choosen Filter doesen't matches with any Events the Page Shows Warning. Finally, Navigation in between Pages is Implemented with Custom Buttons.

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,7 +1,70 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+import EventList from "../../Components/events/event-list";
+import ResultsTitle from "../../Components/events/results-title";
+import ErrorAlert from "../../Components/UI/error-alert";
+import { getFilteredEvents } from "../../dummy-data";
+import Button from "../../Components/UI/button";
+
 export default function FilteredEventsPage() {
+    const router = useRouter();
+    const filterDataSlug = router.query.slug;
+
+    if (!filterDataSlug) {
+        return <p className="center">Loading...</p>;
+    }
+
+    const filteredYear = filterDataSlug[0];
+    const filteredMonth = filterDataSlug[1];
+
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return (
+            <React.Fragment>
+                <ErrorAlert>
+                    <p>Invalid filter. Please Adjust Your Values</p>
+                </ErrorAlert>
+                <div className="center">
+                    <Button link="/events">Show All Events</Button>
+                </div>
+            </React.Fragment>
+        );
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    });
+
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return (
+            <React.Fragment>
+                <ErrorAlert>
+                    <p>No Events found for the Choosen Filter!</p>
+                </ErrorAlert>
+                <div className="center">
+                    <Button link="/events">Show All Events</Button>
+                </div>
+            </React.Fragment>
+        );
+    }
+
+    const date = new Date(numYear, numMonth - 1);
+
     return (
-        <div>
-            <h1>Filtered Events</h1>
-        </div>
+        <React.Fragment>
+            <ResultsTitle date={date} />
+            <EventList items={filteredEvents} />
+        </React.Fragment>
     );
 }
--- pages/events/[eventid].jsx
+++ pages/events/[eventid].jsx
@@ -4,15 +4,20 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../Components/event-detail/event-summary";
 import EventLogistics from "../../Components/event-detail/event-logistics";
 import EventContent from "../../Components/event-detail/event-content";
+import ErrorAlert from "../../Components/UI/error-alert";
 
 export default function EventDetailPage() {
     const router = useRouter();
-    console.log(router.query)
+    console.log(router.query);
     const eventId = router.query.eventid;
     const event = getEventById(eventId);
 
     if (!event) {
-        return <p>No Event Found!!</p>;
+        return (
+            <ErrorAlert>
+                <p>No Event Found!!</p>
+            </ErrorAlert>
+        );
     }
     return (
         <React.Fragment>
