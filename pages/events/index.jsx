5dd92425ea67d0ba69b7f00fb013edd29b9fe711
Apurba Ghosh <new.to.world2003@gmail.com>
2022-06-05 23:35:30 +0530

    Third Lap Refactored the Button Component. Added a Event Search Component which adds a Form for Specifying the Month & the Year for Events Filtering. Styled the form i.e the EventsSearch Component. Also, added a Navigation Functionality to the Filtered Events Page onSubmitting the Filter Form from the All Events Page.

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,10 +1,25 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+
 import EventList from "../../Components/events/event-list";
+import EventsSearch from "../../Components/events/event-search";
 import { getAllEvents } from "../../dummy-data";
 
 export default function AllEventsPage() {
-    const events = getAllEvents()
+    const events = getAllEvents();
+    const router = useRouter();
+
+
+    const findEventsHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`;
+
+        router.push(fullPath)
+    }
+
     return (
         <div>
+            <EventsSearch onSearch={findEventsHandler} />
             <EventList items={events} />
         </div>
     );
